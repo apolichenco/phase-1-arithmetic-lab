e13477deb9ebd56c8fc451962d0f074b02294526
abraham <apolichenco@gmail.com>
2022-06-07 20:17:17 -0400

    commit tree 1cd02098cfccd53b915b611cb159e9dc1bda536d

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const multiply = 31 * 2;
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const random = Math.ceil(Math.random());
+
+const mod = 16 % 6;
+let num3 = 16;
+let num4 = 6;
+
+const max = Math.max(15, 20, 5, 13);
